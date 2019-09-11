d4d48a0d3c5474b65ba276305ba3ccf70630107a
jingyun_liu <jingyun_liu@xinyan.com>
2019-09-11 09:33:48 +0800

    简化项目

--- trust/src/main/java/com/xinyan/trust/controller/ZiJinController.java
+++ trust/src/main/java/com/xinyan/trust/controller/ZiJinController.java
@@ -6,45 +6,46 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
-@Controller("/zijin")
+@RestController
 @Api(value = "ZiJinController|一个用来测试的爬虫")
-@ApiOperation(value = "zijin")
 public class ZiJinController {
     @Autowired
     private ZIjinService zijinService;
 
-    @ApiOperation(value = "cunxunzhong",notes="获取紫金信托存续中的产品")
+    @ApiOperation(value = "cunxunzhong", notes = "获取紫金信托存续中的产品")
     @GetMapping("/cunxunzhong")
-    public String zijin(){
+    public String zijin() {
         //创建一个UUID当作token唯一标识
         String token = UUID.randomUUID().toString();
         this.zijinService.startSpider(token);
         return token;
     }
 
-    @ApiOperation(value="获取爬取任务状态")
-    @ApiImplicitParam(name = "token",value = "唯一标识",required = true,dataType = "string")
+    @ApiOperation(value = "获取爬取任务状态")
+    @ApiImplicitParam(name = "token", value = "唯一标识", required = true, dataType = "string")
     @GetMapping("/getStatus")
-    public String getStatus(@RequestParam(value = "token") String token){
+    public String getStatus(@RequestParam(value = "token") String token) {
         return Status.getValue(this.zijinService.getStatus(token));
     }
 
-    @ApiOperation(value="下载")
-    @ApiImplicitParam(name = "token",value = "唯一标识",required = true,dataType = "string")
+    @ApiOperation(value = "下载")
+    @ApiImplicitParam(name = "token", value = "唯一标识", required = true, dataType = "string")
     @GetMapping("/dowbLoadExcel")
-    public String downLoadExcel(String token, HttpServletResponse response){
-        return this.zijinService.download(token,response);
+    public String downLoadExcel(String token, HttpServletResponse response) {
+        return this.zijinService.download(token, response);
     }
 
+    @ApiOperation(value = "测试使用")
+    @ApiImplicitParam(name = "token", value = "唯一标识", required = true, dataType = "string")
     @GetMapping("/getTest")
-    public String getTest(@RequestParam(value = "token") String token){
+    public String getTest(@RequestParam(value = "token") String token) {
         return Status.getValue(token);
     }
 }
